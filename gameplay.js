4df15a95b188fb5c534207d3ad1803e592075066
abzz.kk <102366893+hypemannnn@users.noreply.github.com>
2024-12-28 10:01:28 +0000

    Add files via upload

---
+++ gameplay.js
@@ -0,0 +1,24 @@
+// AFRAME.registerComponent("collisiondetector",{
+//     init: function(){
+//         counter=0;
+//         this.el.addEventListener("collide",function(e){
+//             e.detail.target.el.remove();
+//             console.log(e.detail.body.el)
+
+//         });
+//         // el is element
+//     },
+
+// });
+
+AFRAME.registerComponent("collisiondetector", {
+    init: function () {
+        this.el.addEventListener("collide", function (e) {
+            console.log("Collision detected with:", e.detail.body.el); // Log the collided entity
+            if (e.detail.body.el) {
+                e.detail.body.el.parentNode.removeChild(e.detail.body.el); // Remove the collided entity
+                console.log("Turtle removed!");
+            }
+        });
+    },
+})
